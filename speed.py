eddb0e2064541ce57ca52434c731123376f811cb
gioGats <ryangiarusso@gmail.com>
2017-05-29 18:49:19 -0500

    Cursory review of performance testing

--- speed.py
+++ speed.py
@@ -2,6 +2,12 @@ from GA import *
 import sys
 from timeit import timeit
 
+# ISSUE Test on realistic size examples.
+# These are all four bits; target_data/films/argo.txt is 138914 characters and 1126304 bits long.
+# If we measure out to 2 decimal places (we actually measure closer to 12),
+# even if there is a 200% difference in timing between 0.002 and 0.001, we will
+# measure them as 0.00.  Use longer and more complex tasks to better test performance.
+
 
 def numpy_func():
     numpyarray = np.array(
@@ -9,7 +15,14 @@ def numpy_func():
           bitstring.BitArray(bin='0110')],
          [bitstring.BitArray(bin='0001'), bitstring.BitArray(bin='0011'), bitstring.BitArray(bin='0111'),
           bitstring.BitArray(bin='1110')]], dtype=object)
+    # ISSUE These are dtype=object, when really the closest approximation would be dtype=bool.
+    # Try both, it might make a difference.
+
     for x in numpyarray:
+        # ISSUE Do an actual operation, like flip all bits
+        # If the function runtime is effectively zero, the system will run at the speed of memory access;
+        # In such a setting, parallelization or vectorization won't help much.  But in practice, our functions
+        # will have a non-zero runtime.  So we should test on a non-zero setting.
         x = x
 
 def numpy_vfunc():
@@ -22,6 +35,12 @@ def numpy_vfunc():
     def func(x):
         return x
 
+    # ISSUE These results don't make sense.
+    # Vectorized functions should be faster than for-loop functions.
+    # This could be from using just an identity function, but also could
+    # be because there's fairly substantial overhead coming from np.vectorize.
+    # To counteract this, don't redefine and vectorize "func" every iteration,
+    # do it once and then use that vectorized function repeatidly.
     vfunc = np.vectorize(func)
     vfunc(numpyarray)
 
@@ -60,6 +79,11 @@ print('python with for loop:', timeit('py_func()', 'from __main__ import py_func
 # result shocked me, to say the least.
 # If these tests don't make sense or you'd like to try some other tests, by all means go ahead and defintely send me
 # the results! Otherwise, if you're okay with it I would just ditch numpy.
+#
+# ISSUE Is there a way to support a collection of bitstring.BitArray's that's not a Python list?
+# I'm really hesitant to use lists--their implementation is very inefficient for a usecase like this.
+# Try rerunning both speed and memory calculations on a more realistic trial set to see, but I'm worried
+# the list is just going to explode in size.
 #
 # When I ran this on my laptop, I got the following scores:
 #
